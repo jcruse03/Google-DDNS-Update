b5f90340d515bc1eea44becee0afa42e4fb42a82
jcruse03 <jcruse03@gmail.com>
2017-09-06 19:19:51 -0400

    Add response handler

--- ddns-update.py
+++ ddns-update.py
@@ -20,6 +20,7 @@ class Record:
         self.sub_domain = sub_domain
         self.user = user
         self.password = password
+        self.active = True
 
     def show(self):
         """print the record"""
@@ -27,7 +28,47 @@ class Record:
 
     def get_ip(self):
         """Returns the current ip address of self.sub_domain"""
-        return socket.gethostbyname(self.sub_domain)
+        try:
+            return socket.gethostbyname(self.sub_domain)
+        except socket.gaierror:
+            logging.error('Unknown hostname ' + self.sub_domain + ' Record Deactivated')
+            self.active = False
+            return 'unknown_ip'
+
+    def handle_response(self, page):
+        """Handle responses from domains.google.com
+        Keyword arguments:
+        page -- the response string
+        """
+        page = str(page)[1:].replace("'", '')
+        if page[0:4] == 'good':
+            logging.info('Success: good response = ' + page)
+        elif page[0:5] == 'nochg':
+            logging.warning('Success: no change response = ' + page)
+        elif page == 'nohost':
+            logging.error(('Error: The hostname does not exist or Dynamic DNS is not ' +
+                           'enabled. RECORD DEACTIVATED. response = ' + page))
+            self.active = False
+        elif page == 'badauth':
+            logging.error(('Error: username / password combination is not valid for ' +
+                           'the specified host. RECORD DEACTIVATED. response = ' + page))
+            self.active = False
+        elif page == 'notfqdn':
+            logging.error(('Error: hostname is not a valid fully-qualified domain ' +
+                           'name. RECORD DEACTIVATED. response = ' + page))
+            self.active = False
+        elif page == 'badagent':
+            logging.error(('Error: Making bad requests. May be shut down for abuse. ' +
+                           'RECORD DEACTIVATED. response = ' + page))
+            self.active = False
+        elif page == 'abuse':
+            logging.error(('Error: hostname blocked due to failure to interpret ' +
+                           'previous responses correctly. RECORD DEACTIVATED. response = ' + page))
+            self.active = False
+        elif page == '911':
+            logging.error(('Error: An error happened on our end. Retry. response = ' + page))
+        else:
+            logging.error('Unknown response. response = ' + page)
 
     def update_ip(self, ip_addr):
         """Sends a post request to https://domains.google.com and updates the DDNS ip address.
@@ -46,9 +87,7 @@ class Record:
         req.add_header('Authorization', 'Basic %s' % base64_user_pass)
         response = urllib.request.urlopen(req)
         page = response.read()
-        logging.info(page)
-        if DEBUG:
-            print(page)
+        self.handle_response(page)
 
 class Domain:
     def __init__(self):
@@ -66,9 +105,9 @@ class Domain:
 
 def get_my_ip():
     """Returns the current public ip address"""
-    info = str(urllib.request.urlopen(
+    page = str(urllib.request.urlopen(
         "https://api.ipify.org?format=txt").read()).replace('b', '').replace("'", '')
-    return info
+    return page
 
 def read_conf():
     """Reads file "/etc/ddns-update.conf and returns a Domain object"""
@@ -94,7 +133,7 @@ def read_conf():
             else:
                 logging.warning('invalid entry on line ' + str(counter) +
                                 ' in /etc/ddns-update.conf. Must be 3 arguments: ' +
-                                'subdomain user password, or TICKTIME NUM ')
+                                'subdomain user password, or 2 agruments: TICKTIME NUM')
             counter += 1
         file.close()
     except OSError:
@@ -114,16 +153,26 @@ COUNT = 0
 
 while True:
     for record in DOMAINS.records:
+        inactive_records = 0
         try:
-            current_ip = get_my_ip()
-            dns_ip = record.get_ip()
+            current_ip = ''
+            dns_ip = ''
+            if record.active:
+                current_ip = get_my_ip()
+                dns_ip = record.get_ip()
             if DEBUG and COUNT < len(DOMAINS.records):
                 record.update_ip(socket.gethostbyname('google.com'))
                 COUNT += 1
-            if current_ip != dns_ip:
+            if record.active and current_ip != dns_ip:
                 logging.info('Updating: ' + record.sub_domain +
                              ' from: ' + dns_ip + ' to: ' + current_ip)
                 record.update_ip(current_ip)
+            elif not record.active:
+                logging.info('Record ' + record.sub_domain + ' is inactive.')
+                inactive_records += 1
+                if inactive_records >= len(DOMAINS.records):
+                    logging.error('Error: all records inactive. exiting')
+                    exit(1)
             else:
                 logging.info('no change on ' + record.sub_domain +
                              ' dns_ip = ' + dns_ip + ', current_ip= ' + current_ip)
