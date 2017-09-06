48318b3b6fba1df230baa2c7952d1980787dec13
jcruse03 <jcruse03@gmail.com>
2017-09-05 21:26:20 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+systemctl stop ddns-update.service | echo
 cp ddns-update.py /usr/bin/ | echo
-cp ddns-update.service /lib/systemd/system/ | echo
+cp -n ddns-update.service /lib/systemd/system/ | echo
 cp -n ddns-update.conf /etc/ | echo
+systemctl start ddns-update.service | echo
