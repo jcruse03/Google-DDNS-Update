714f4991a466594393b79b46d69b6f969da1c156
jcruse03 <jcruse03@gmail.com>
2017-09-06 16:03:25 -0400

    Update mac-setup.sh

--- mac-setup.sh
+++ mac-setup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-launchctl stop /Library/LaunchDaemons/ddns-update.plist | echo
 cp ddns-update.py /usr/local/bin/ | echo
 cp -n ddns-update.plist /Library/LaunchDaemons/ | echo
 cp -n ddns-update.conf /etc/ | echo
-launchctl start /Library/LaunchDaemons/ddns-update.plist | echo
 echo Done!
