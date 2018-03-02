7ef2102ae98ff1af08cee8f6079e03e2ac557c5a
jcruse03 <jcruse03@gmail.com>
2018-03-02 10:27:22 -0500

    Update setup-rc.sh

--- setup-rc.sh
+++ setup-rc.sh
@@ -4,6 +4,7 @@ cp ddns-update /usr/bin/ | echo
 chmod 744 /usr/bin/ddns-update | echo
 cp -n ddns-update.openrcservice /etc/init.d/ | echo
 mv /etc/init.d/ddns-update.openrcservice /etc/init.d/ddns-update.service | echo
+chmod 744 /etc/init.d/ddns-update.service
 cp -n ddns-update.conf /etc/ | echo
 rc-service ddns-update.service start | echo
 echo Done!
