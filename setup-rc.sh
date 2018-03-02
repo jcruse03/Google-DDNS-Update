8660cded99ce23dfd448e6c00122fef51ad722dc
jcruse03 <jcruse03@gmail.com>
2018-03-02 12:11:01 -0500

    Update setup-rc.sh

--- setup-rc.sh
+++ setup-rc.sh
@@ -7,10 +7,10 @@ mv /etc/init.d/ddns-update.openrcservice /etc/init.d/ddns-update.service | echo
 chmod 744 /etc/init.d/ddns-update.service
 rc-update add ddns-update.service boot
 cp -n ddns-update.conf /etc/ | echo
-rm /run/ddns-update.service.pid
 sleep 2
 pid=$(cat /run/ddns-update.service.pid)
 kill $pid
+rm /run/ddns-update.service.pid
 sleep 1
 rc-service ddns-update.service start | echo
 echo Done!
